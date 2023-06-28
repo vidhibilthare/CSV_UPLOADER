1dd2a51620cc789dd092bad74252fe67892f3bdb
vidhibilthare <vidhibilthare@gmail.com>
2023-06-28 20:27:55 +0530

    update_1

--- upload_csv/home/urls.py
+++ upload_csv/home/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('',views.home, name='home'),
     path('admin_all/',views.admin, name='admin_page'),
     path('downloafde/<int:file_id>/',views.downloade_file, name='downloade_file'),
-    path('view/<int:file_id/',views.view_file, name='view_file')
+    path('view/<int:file_id>/',views.view_file, name='view_file')
     
 ]
 
